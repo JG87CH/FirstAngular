302181cb0e9fd07c18da1fd068d1202ee83fc15f
JG87CH <jangre@gmx.ch>
2021-12-17 19:05:05 +0100

    adding open api generated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiModule, BASE_PATH} from "./openapi-gen";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule
   ],
-  providers: [],
+  providers: [
+    { provide: BASE_PATH, useValue: environment.API_BASE_PATH }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
